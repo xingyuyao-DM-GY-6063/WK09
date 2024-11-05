36340d4aacb835dcc84db73cfb58f985f604fa08
xingyu yao <xy2779@nyu.edu>
2024-11-04 21:07:49 -0500

    Update sketch.js

--- 3/sketch.js
+++ 3/sketch.js
@@ -4,10 +4,11 @@ let gridDim = 16;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
 
   for (let x = 0; x < width; x += gridDim) {
     for (let y = 0; y < height; y += gridDim) {
-      let mc = 255;
+      let mc = 255 * noise(x/100,y/100);
       fill(mc);
       rect(x, y, gridDim);
     }
